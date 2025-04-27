09174d9a3a2dca67279f37af94a7c2ae673fac38
Phyo Theingi <tiennvanng@gmail.com>
2025-04-27 19:13:21 +0700

    Add user code files: Book.java, PurchaseHistory.java, User.java

--- src/th/ac/cmkl/bookrec/model/User.java
+++ src/th/ac/cmkl/bookrec/model/User.java
@@ -1,68 +1,120 @@
 package th.ac.cmkl.bookrec.model;
 
-import th.ac.cmkl.bookrec.model.Book;
-import th.ac.cmkl.bookrec.model.PurchaseHistory;
-import th.ac.cmkl.bookrec.search.SearchEngine;
-import th.ac.cmkl.bookrec.recommender.RecommendationEngine;
 import th.ac.cmkl.bookrec.recommender.HybridRecommender;
+import th.ac.cmkl.bookrec.recommender.RecommendationEngine;
+import th.ac.cmkl.bookrec.search.SearchEngine;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 /**
- * Represents a user in the system.
+ * User
+ *
+ * Represents a user in the system with capabilities to login,
+ * search for books, receive recommendations, and view purchase history.
+ *
+ * Created by Taha Keler and Phyo Theingi
+ * April 27, 2025
  */
-public class User {
 
+public class User
+{
+    /** Username for login */
     private String username;
+
+    /** Password for login */
     private String password;
+
+    /** List of user's purchase histories */
     private List<PurchaseHistory> purchaseHistories;
 
-    public User(String username, String password) {
+    /**
+     * Constructs a User with a username and password.
+     *
+     * @param username The user's username.
+     * @param password The user's password.
+     */
+    public User(String username, String password)
+    {
         this.username = username;
         this.password = password;
         this.purchaseHistories = new ArrayList<>();
     }
 
-    public boolean login(String username, String password) {
+    /**
+     * Validates the user's login credentials.
+     *
+     * @param username Input username.
+     * @param password Input password.
+     * @return true if credentials match; false otherwise.
+     */
+    public boolean login(String username, String password)
+    {
         return this.username.equals(username) && this.password.equals(password);
     }
 
-    public void buyBook(Book book) {
-        purchaseHistories.add(new PurchaseHistory(book, new java.util.Date()));
+    /**
+     * Records a book purchase into the user's history.
+     *
+     * @param book The book being purchased.
+     */
+    public void buyBook(Book book)
+    {
+        purchaseHistories.add(new PurchaseHistory(book, new Date()));
     }
 
     /**
-     * Searches books using the SearchEngine utility.
+     * Searches for books using a keyword through the SearchEngine utility.
      *
-     * @param books the list of all books
-     * @param query the keyword to search
-     * @return matching books
+     * @param books The list of available books.
+     * @param query The keyword to search.
+     * @return List of books matching the query.
      */
-    public List<Book> searchBooks(List<Book> books, String query) {
+    public List<Book> searchBooks(List<Book> books, String query)
+    {
         return SearchEngine.search(books, query);
     }
 
     /**
-     * Gets recommendations using the default hybrid strategy.
+     * Retrieves book recommendations using the hybrid recommendation strategy.
      *
-     * @param allBooks the complete list of books
-     * @return a list of recommended books
+     * @param allBooks The complete list of books.
+     * @return A list of recommended books.
      */
-    public List<Book> getRecommendations(List<Book> allBooks) {
+    public List<Book> getRecommendations(List<Book> allBooks)
+    {
         RecommendationEngine engine = new HybridRecommender(allBooks);
         return engine.getRecommendations(this);
     }
 
-    public List<PurchaseHistory> viewPurchaseHistory() {
+    /**
+     * Retrieves the user's purchase history.
+     *
+     * @return List of PurchaseHistory objects.
+     */
+    public List<PurchaseHistory> viewPurchaseHistory()
+    {
         return purchaseHistories;
     }
 
-    public String getUsername() {
+    /**
+     * Gets the user's username.
+     *
+     * @return Username.
+     */
+    public String getUsername()
+    {
         return username;
     }
 
-    public List<PurchaseHistory> getPurchaseHistories() {
+    /**
+     * Gets the user's full purchase history.
+     *
+     * @return List of PurchaseHistory.
+     */
+    public List<PurchaseHistory> getPurchaseHistories()
+    {
         return purchaseHistories;
     }
 }
